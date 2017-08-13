f71521c80a1d341f052762a3a7e760064b777f01
arashout <arash.out@gmail.com>
2017-08-12 18:42:58 -0700

    Added api handler and bundled some code for driver functions

--- brightspace_client.go
+++ brightspace_client.go
@@ -11,9 +11,10 @@ import (
 
 // BrightspaceClient ...
 type BrightspaceClient struct {
-	credentials *Credentials
-	client      *http.Client
-	state       string
+	credentials   *Credentials
+	client        *http.Client
+	clientBaseURL string
+	state         string
 }
 
 const (
@@ -23,9 +24,10 @@ const (
 )
 
 // CreateBrightspaceClient ...
-func CreateBrightspaceClient(cred *Credentials) *BrightspaceClient {
+func CreateBrightspaceClient(credentials *Credentials, clientBaseURL string) *BrightspaceClient {
 	var brightspaceClient BrightspaceClient
-	brightspaceClient.credentials = cred
+	brightspaceClient.credentials = credentials
+	brightspaceClient.clientBaseURL = clientBaseURL
 	brightspaceClient.client = &http.Client{}
 
 	var err error
@@ -46,7 +48,7 @@ func (bsc *BrightspaceClient) AuthHandler(w http.ResponseWriter, r *http.Request
 	}
 	q := req.URL.Query()
 	q.Add("response_type", "code")
-	q.Add("redirect_uri", createRedirectTo(clientURLBase, "callback"))
+	q.Add("redirect_uri", createRedirectTo(bsc.clientBaseURL, "callback"))
 	q.Add("client_id", bsc.credentials.ClientID)
 	q.Add("scope", bsc.credentials.Scope)
 	q.Add("state", bsc.state)
@@ -57,34 +59,30 @@ func (bsc *BrightspaceClient) AuthHandler(w http.ResponseWriter, r *http.Request
 
 // AuthCallbackHandler ...
 func (bsc *BrightspaceClient) AuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	bsc.credentials.AuthCode = r.URL.Query().Get("code")
-
+	// Avoid CSRF attacks by checking generated state
 	if bsc.state != r.URL.Query().Get("state") {
 		log.Fatal("States don't match!")
 	}
+	// Get the authorization code
+	bsc.credentials.AuthCode = r.URL.Query().Get("code")
 
+	// Send a request asking for access token and refresh token using authorization code
 	payload := url.Values{}
 	payload.Add("grant_type", "authorization_code")
 	// TODO: Create route list so I don't have to hardcode
-	payload.Add("redirect_uri", createRedirectTo(clientURLBase, "callback"))
+	payload.Add("redirect_uri", createRedirectTo(bsc.clientBaseURL, "callback"))
 	payload.Add("code", bsc.credentials.AuthCode)
 
-	req, err := http.NewRequest("POST", tokenEndpoint, strings.NewReader(payload.Encode()))
-	req.SetBasicAuth(cred.ClientID, cred.ClientSecret)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
 	// The response contains the access token and refresh token
-	resp, err := bsc.client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+	resp := bsc.sendAuthorizedRequestWithPayload("POST", tokenEndpoint, payload)
 	if resp.StatusCode != http.StatusOK {
 		log.Fatal(resp.Status)
 	}
 
+	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
 
 	var token BrightspaceToken
@@ -95,8 +93,91 @@ func (bsc *BrightspaceClient) AuthCallbackHandler(w http.ResponseWriter, r *http
 	}
 	bsc.credentials.Token = &token
 
+	// Store data in a credential file to use refresh token for next time
 	StructToJSONFile(bsc.credentials, credFilePath)
-	log.Println(JSONStringify(bsc.credentials.Token))
+
+	log.Printf("New Access/Refresh Token using Authorization Code:\n%s", JSONStringify(bsc.credentials.Token))
+
+	// Redirect back to index page
+	http.Redirect(w, r, createRedirectTo(bsc.clientBaseURL, "/"), 301)
+}
+
+// RefreshHandler ...
+func (bsc *BrightspaceClient) RefreshHandler(w http.ResponseWriter, r *http.Request) {
+	// Send a request asking for new access token and refresh token using refresh token
+	payload := url.Values{}
+	payload.Add("grant_type", "refresh_token")
+	payload.Add("refresh_token", bsc.credentials.Token.RefreshToken)
+	payload.Add("scope", bsc.credentials.Scope)
+
+	// The response contains the access token and refresh token
+	resp := bsc.sendAuthorizedRequestWithPayload("POST", tokenEndpoint, payload)
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var token BrightspaceToken
+
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Replace used up token with new token
+	bsc.credentials.Token = &token
+
+	// Store data in a credential file to use refresh token for next time
+	StructToJSONFile(bsc.credentials, credFilePath)
+
+	log.Printf("New Access/Refresh Token using Refresh Token:\n%s", JSONStringify(bsc.credentials.Token))
+}
+
+// APIHandler ...
+func (bsc *BrightspaceClient) APIHandler(w http.ResponseWriter, r *http.Request) {
+	baseRoute := "/d2l/api/lp/"
+	version := "1.13"
+	apiCommand := "/users/"
+	fullURL := bsc.credentials.HostURL + baseRoute + version + apiCommand
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", cred.Token.TokenType+" "+cred.Token.AccessToken)
+
+	resp, err := bsc.client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: Deal with the case of a access token being expired!
+	if resp.StatusCode != 200 {
+		log.Panic(err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
+
+}
+
+func (bsc *BrightspaceClient) sendAuthorizedRequestWithPayload(method string, url string, payload url.Values) *http.Response {
+	req, err := http.NewRequest("POST", tokenEndpoint, strings.NewReader(payload.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth(cred.ClientID, cred.ClientSecret)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := bsc.client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp
 }
 
 func createRedirectTo(basePath string, relativePath string) string {
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -16,12 +14,11 @@ type Credentials struct {
 	ClientSecret string `json:"clientSecret"`
 	HostURL      string `json:"hostURL"`
 	Scope        string `json:"scope"`
-	State        string
-	AuthCode     string
+	AuthCode     string `json:"authorizationCode"`
 	Token        *BrightspaceToken
 }
 
-//  BrightspaceToken ... to get access token
+// BrightspaceToken ... To get access token
 type BrightspaceToken struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
@@ -31,9 +28,7 @@ type BrightspaceToken struct {
 
 const (
 	credFilePath = ".devenv.json"
-	port         = "3001"
-	// Would prefer to check server address...
-	clientURLBase = "https://localhost:" + port
+	clientPort   = "3001"
 )
 
 var cred Credentials
@@ -45,53 +40,31 @@ func main() {
 	err := httpscerts.Check("cert.pem", "key.pem")
 	// If they are not available, generate new ones.
 	if err != nil {
-		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:"+port)
+		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:"+clientPort)
 		if err != nil {
 			log.Fatal("Error: Couldn't create https certs.")
 		}
 	}
 
-	bsc := CreateBrightspaceClient(&cred)
+	bsc := CreateBrightspaceClient(&cred, "https://localhost:"+clientPort)
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("dist")))
 	mux.HandleFunc("/auth", bsc.AuthHandler)
 	mux.HandleFunc("/callback", bsc.AuthCallbackHandler)
-	mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/refresh", bsc.RefreshHandler)
+	mux.HandleFunc("/api", bsc.APIHandler)
 
 	srv := &http.Server{
-		Addr:         ":" + port,
+		Addr:         ":" + clientPort,
 		Handler:      mux,
 		ReadTimeout:  2 * time.Second,
 		WriteTimeout: 2 * time.Second,
 		IdleTimeout:  2 * time.Second,
 	}
-	log.Printf("Listening on port: %s", port)
+	log.Printf("Listening to client on port: %s", clientPort)
 	err = srv.ListenAndServeTLS("cert.pem", "key.pem")
 	if err != nil {
 		log.Println("Fail to bind port: ", err)
 	}
-}
-
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	baseRoute := "/d2l/api/lp/"
-	version := "1.13"
-	apiCommand := "/users/"
-	fullURL := cred.HostURL + baseRoute + version + apiCommand
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", fullURL, nil)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", cred.Token.TokenType+" "+cred.Token.AccessToken)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
 }
